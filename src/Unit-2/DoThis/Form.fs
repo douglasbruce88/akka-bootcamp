b7a3038d79d4a72acaf6f8fb9572b5a2ef7ac4ed
Doug Bruce <douglas.bruce@redington.co.uk>
2016-05-27 15:27:07 +0100

    Unit 2, Lesson 2

--- src/Unit-2/DoThis/Form.fs
+++ src/Unit-2/DoThis/Form.fs
@@ -2,11 +2,10 @@
 
 open Akka.Actor
 open Akka.FSharp
+open Akka.Util.Internal
 open System.Drawing
 open System.Windows.Forms
 open System.Windows.Forms.DataVisualization.Charting
-open Akka.Util.Internal
-
 
 [<AutoOpen>]
 module Form = 
@@ -15,17 +14,39 @@ module Form =
     let chartArea1 = new ChartArea(Name = "ChartArea1")
     let legend1 = new Legend(Name = "Legend1")
     let series1 = new Series(Name = "Series1", ChartArea = "ChartArea1", Legend = "Legend1")
-    sysChart.BeginInit ()
-    form.SuspendLayout ()
+    let btnCpu = new Button(Name = "btnCpu", Text = "CPU (ON)", Location = Point(562, 274), Size = Size(110, 41), TabIndex = 1, UseVisualStyleBackColor = true)
+    let btnMemory = new Button(Name = "btnMemory", Text = "MEMORY (OFF)", Location = Point(562, 321), Size = Size(110, 41), TabIndex = 2, UseVisualStyleBackColor = true)
+    let btnDisk = new Button(Name = "btnDisk", Text = "DISK (OFF)", Location = Point(562, 368), Size = Size(110, 41), TabIndex = 3, UseVisualStyleBackColor = true)
+    
+    sysChart.BeginInit()
+    form.SuspendLayout()
     sysChart.ChartAreas.Add chartArea1
     sysChart.Legends.Add legend1
-    sysChart.Series.Add series1
+    form.Controls.Add btnCpu
+    form.Controls.Add btnMemory
+    form.Controls.Add btnDisk
     form.Controls.Add sysChart
-    sysChart.EndInit ()
+    sysChart.EndInit()
     form.ResumeLayout false
-
-    let load (myActorSystem:ActorSystem) = 
-        let chartActor = spawn myActorSystem "charting" (actorOf (Actors.chartingActor sysChart))
-        let series = ChartDataHelper.randomSeries ("FakeSeries1" ) None None
-        chartActor <! InitializeChart(Map.ofList [(series.Name, series)])
+    
+    let load (myActorSystem : ActorSystem) = 
+        let chartActor = spawn myActorSystem "charting" (Actors.chartingActor sysChart)
+        let coordinatorActor = spawn myActorSystem "counters" (Actors.performanceCounterCoordinatorActor chartActor)
+        
+        let toggleActors = 
+            Map.ofList 
+                [ (CounterType.Cpu, 
+                   spawnOpt myActorSystem "cpuCounter" (Actors.buttonToggleActor coordinatorActor btnCpu CounterType.Cpu false) [ SpawnOption.Dispatcher("akka.actor.synchronized-dispatcher") ]) // CPU Toggle Actor
+                  
+                  (CounterType.Memory, 
+                   spawnOpt myActorSystem "memoryCounter" (Actors.buttonToggleActor coordinatorActor btnMemory CounterType.Memory false) 
+                       [ SpawnOption.Dispatcher("akka.actor.synchronized-dispatcher") ]) // Memory Toggle Actor
+                  
+                  (CounterType.Disk, 
+                   spawnOpt myActorSystem "diskCounter" (Actors.buttonToggleActor coordinatorActor btnDisk CounterType.Disk false) [ SpawnOption.Dispatcher("akka.actor.synchronized-dispatcher") ]) // Disk Toggle Actor
+                                                                                                                                                                                                     ]
+        toggleActors.[CounterType.Cpu] <! Toggle
+        btnCpu.Click.Add(fun _ -> toggleActors.[CounterType.Cpu] <! Toggle)
+        btnMemory.Click.Add(fun _ -> toggleActors.[CounterType.Memory] <! Toggle)
+        btnDisk.Click.Add(fun _ -> toggleActors.[CounterType.Disk] <! Toggle)
         form
